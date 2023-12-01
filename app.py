e34a50ab40a9a9a9b51e3b7ce3706bff907f5194
eduardhabryd <eduardhabryd@gmail.com>
2023-12-01 23:46:00 +0200

    fix callback to use proper argument

--- app.py
+++ app.py
@@ -8,21 +8,22 @@ from bot import main as bot_main
 
 
 async def main():
-    connection = await get_connection()
-    channel = await connection.channel()
+    async with get_connection() as connection:
+        channel = await connection.channel()
 
-    async def callback(ch, method, properties, body):
-        body = str(body.decode('utf-8'))
-        method, body = body.split(' ', 1)
-        print(f"Method: {method}, Last Message: {body}", flush=True)
-        if method == "send":
-            pass
+        async def callback(message: aiormq.abc.DeliveredMessage):
+            body = str(message.body.decode('utf-8'))
+            method, body = body.split(' ', 1)
+            print(f"Method: {method}, Last Message: {body}", flush=True)
+            if method == "send":
+                pass
 
-    # Use await channel.declare_queue instead of await channel.queue_declare
-    declare_ok = await channel.queue_declare('hello')
-    consume_ok = await channel.basic_consume(declare_ok.queue, callback, no_ack=True)
+        declare_ok = await channel.queue_declare('hello')
+        consume_ok = await channel.basic_consume(declare_ok.queue, callback, no_ack=True)
 
-    print('[*] Waiting for messages. To exit press CTRL+C', flush=True)
+        print('[*] Waiting for messages. To exit press CTRL+C', flush=True)
+
+        await asyncio.Event().wait()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
